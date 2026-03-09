9d4914df3a178e76f663e27fbfbfa30fb05413af
Zain4391 <manutdfast91@gmail.com>
2026-03-10 04:11:35 +0500

    Auth routes: Guest Guard add.

--- src/app/features/auth/auth.route.ts
+++ src/app/features/auth/auth.route.ts
@@ -1,4 +1,5 @@
 import { Routes } from "@angular/router";
+import { guestGuard } from "../../core/guards/auth.guard";
 
 export const AUTH_ROUTES: Routes = [
     {
@@ -8,6 +9,7 @@ export const AUTH_ROUTES: Routes = [
     },
     {
         path: 'login',
+        canActivate: [guestGuard],
         loadComponent: () => import('./components/login.component').then(m => m.LoginComponent)
     }
 ];
